1b941a124095c0527e5c715375a78fb2b370d506
siddharth <siddharthb1604@gmail.com>
2021-06-06 21:44:00 +0530

    personalised

--- src/index.js
+++ src/index.js
@@ -1,35 +1,4 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import * as serviceWorker from './serviceWorker';
-import './static/css/normalizer.css'
-
-export default class App extends Component {
-    render() {
-        return (
-            <Switch>
-                <Route exact path="/" render={() => (
-                    <div>
-                        سلام
-                    </div>
-                )}/>
-                {/*<Route exact path="/sth" component={}/>*/}
-            </Switch>
-        )
-    }
-}
-
-
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    , document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<h1>Happy hacking!!</h1>, document.getElementById("root"));
